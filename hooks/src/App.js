4febd9bb0d2c6c6ef0017ac19623068a4c5143b9
Sadhna kumari <sadhnakhairia1998@gmail.com>
2022-10-07 18:14:42 +0530

    useState with object

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,13 +1,15 @@
 import { CountClass } from "./components/CountClass";
 import { CounterHooks } from "./components/CounterHooks";
 import { CountHooks } from "./components/CountHooks";
+import { StateWithObject } from "./components/StateWithObject";
 
 export function App() {
   return (
     <div>
       {/* <CountClass/> */}
       {/* <CountHooks/> */}
-      <CounterHooks/>
+      {/* <CounterHooks/> */}
+      <StateWithObject/>
     </div>
   );
 }
